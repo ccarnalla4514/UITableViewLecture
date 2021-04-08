0c4f714458ff958ec4e812cf63a9bf8e9d24139c
Christian Carnalla <ccarnalla4514@stu.d214.org>
2021-04-08 15:13:34 -0500

    finished

--- UITableViewLecture/ViewController.swift
+++ UITableViewLecture/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    let cities = ["Chicago", "Dallas", "Paris", "Rome", "London"]
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myTableView.delegate = self
+        myTableView.dataSource = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let myCell = myTableView.dequeueReusableCell(withIdentifier: "myCell")
+       
+       // myCell?.textLabel!.text = "This is a Cell"
+// return myCell!
+let myCell = myTableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+myCell.textLabel!.text = cities[indexPath.row]
+        
+        
+        return myCell
+        
+    }
+    
 }
 
